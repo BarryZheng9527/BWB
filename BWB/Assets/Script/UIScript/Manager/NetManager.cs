ac41154d70283c185a149561778f25039bc9b988
BarryZheng <zhengbolun@126.com>
2016-10-11 14:08:39 +0800

    合并改动

--- BWB/Assets/Script/UIScript/Manager/NetManager.cs
+++ BWB/Assets/Script/UIScript/Manager/NetManager.cs
@@ -84,6 +84,8 @@ public class NetManager
         RegisterResponse response = new RegisterResponse();
         if (name == "bwb" && password == "bwb")
         {
+			response.name = name;
+			response.password = password;
         }
         else
         {
