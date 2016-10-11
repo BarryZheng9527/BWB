cbf541c7e74b4b3341af167f36f4bff8dcff64ae
BarryZheng <zhengbolun@126.com>
2016-10-11 14:01:14 +0800

    Revert "合并修改"
    This reverts commit 15b22666e46e93fb28fb8a560c52cb1035966f96.

--- BWB/Assets/Script/UIScript/Constant/ErrorConstant.cs
+++ BWB/Assets/Script/UIScript/Constant/ErrorConstant.cs
@@ -4,7 +4,7 @@
 
     public const int ERROR_0 = 0; //成功
     public const int ERROR_100001 = 100001; //金币不足
-    public const int ERROR_100002 = 100002; //登陆失败
+    public const int ERROR_100002 = 100002; //进入游戏失败
     public const int ERROR_100003 = 100003; //角色名或密码不能为空
     public const int ERROR_100004 = 100004; //两次输入密码不一致
     public const int ERROR_100005 = 100005; //注册失败
